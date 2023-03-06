2de33960f1a576018a40cbbd42981b525b4cf1eb
German <gedaniel1@hotmail.com>
2023-03-06 18:23:13 -0300

    categ dinamicas y search cruzado

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,33 +1,61 @@
 // console.table(data.events);
 
 // Para el Home
+
+// Para las categorías
+function printCategory() {
+    let allCateg = [];
+    let allCategPrint = [];
+    let container = document.getElementById("categoryId");
+
+    data.events.forEach(element => {
+        if (!allCateg.includes(element.category)) {
+            allCateg.push(element.category)
+            allCategPrint.push(templateCategory(element.category))
+        }
+    });
+
+    container.innerHTML = allCategPrint.join("");
+}
+printCategory();
+
+
+
+// Para las cards
 function printAllEvents() {
     let allCards = [];
     let container = document.getElementById("card-home");
 
     for (let event of data.events) {
-        let card = `
-        <article class="card">
-            <figure>
-                <img class="card-image" src="${event.image}" alt="${event.name}">
-            </figure>
-            <h2>${event.name}</h2>
-            <p>${event.description}</p>
-            <div class="base-card">
-                <p>Price $${event.price}</p>
-                <a class="but-view" href="./details.html?id=${event._id}">View more</a>
-            </div> 
-        </article>
-        `
-        allCards.push(card);
+        allCards.push(templateCard(event.image, event.name, event.description, event.price, event._id));
     }
     container.innerHTML = allCards.join("");
 }
 printAllEvents();
 
 
+function templateCard (image, name, description, price, id) {
+    return `
+        <article class="card">
+            <figure>
+                <img class="card-image" src="${image}" alt="${name}">
+            </figure>
+            <h2>${name}</h2>
+            <p>${description}</p>
+            <div class="base-card">
+                <p>Price $${price}</p>
+                <a class="but-view" href="./details.html?id=${id}">View more</a>
+            </div> 
+        </article>
+        `
+        // también se puede poner todo en una variable y luego retornarla
+}
 
-
+function templateCategory(category) {
+    return `<label for="${category}">
+    <input onclick="captureData()" class="check" type="checkbox" name="choose" id="${category}" value="${category}">
+    ${category}</label>`
+}
 
 
 
