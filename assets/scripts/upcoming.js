fd5e0dcdfcb40e3fcfb6d0ac5626f99b0a86ec59
German <gedaniel1@hotmail.com>
2023-03-08 17:17:30 -0300

    task3 fin - dinamismo, search cruzado y details home, past y upcoming

--- assets/scripts/upcoming.js
+++ assets/scripts/upcoming.js
@@ -17,31 +17,3 @@ function createUpcomingEventsArray(lista) {
 
 // Llamo a la función para que me genere el array filtrado
 createUpcomingEventsArray(data.events)
-
-// function printUpcomingEvents(lista) {
-//     let upEvCards = [];
-//     let containerUpEv = document.getElementById("card-UpEvents");
-
-//     for (let event of lista) {
-//         if (event.date > data.currentDate) {
-//             // let card = `
-//             // <article class="card">
-//             //     <figure>
-//             //         <img class="card-image" src="${event.image}" alt="${event.name}">
-//             //     </figure>
-//             //     <h2>${event.name}</h2>
-//             //     <p>${event.description}</p>
-//             //     <div class="base-card">
-//             //         <p>Price $${event.price}</p>
-//             //         <a class="but-view" href="./details.html?id=${event._id}">View more</a>
-//             //     </div> 
-//             // </article>
-//             // `
-//             upEvCards.push(templateCard(event.image, event.name, event.description, event.price, event._id));
-//         }
-//     }
-//     containerUpEv.innerHTML = upEvCards.join("");
-// }
-// printUpcomingEvents(data.events);
-
-
