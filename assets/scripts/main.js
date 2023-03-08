cf093481d6d8fe33cdcf052defba6df3b48d3d57
German <gedaniel1@hotmail.com>
2023-03-07 22:32:48 -0300

    categ dinamicas y search cruzado para home, past y upcoming

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,64 +2,14 @@
 
 // Para el Home
 
-// Para las categorías
-function printCategory() {
-    let allCateg = [];
-    let allCategPrint = [];
-    let container = document.getElementById("categoryId");
+// Para imprimir las categorías
+printCategory("categoryId");
 
-    data.events.forEach(element => {
-        if (!allCateg.includes(element.category)) {
-            allCateg.push(element.category)
-            allCategPrint.push(templateCategory(element.category))
-        }
-    });
+// Establezco el array que voy a usar, en este caso el array completo
+captureList = data.events;
 
-    container.innerHTML = allCategPrint.join("");
-}
-printCategory();
-
-
-
-// Para las cards
-function printAllEvents() {
-    let allCards = [];
-    let container = document.getElementById("card-home");
-
-    for (let event of data.events) {
-        allCards.push(templateCard(event.image, event.name, event.description, event.price, event._id));
-    }
-    container.innerHTML = allCards.join("");
-}
-printAllEvents();
-
-
-function templateCard (image, name, description, price, id) {
-    return `
-        <article class="card">
-            <figure>
-                <img class="card-image" src="${image}" alt="${name}">
-            </figure>
-            <h2>${name}</h2>
-            <p>${description}</p>
-            <div class="base-card">
-                <p>Price $${price}</p>
-                <a class="but-view" href="./details.html?id=${id}">View more</a>
-            </div> 
-        </article>
-        `
-        // también se puede poner todo en una variable y luego retornarla
-}
-
-function templateCategory(category) {
-    return `<label for="${category}">
-    <input onclick="captureData()" class="check" type="checkbox" name="choose" id="${category}" value="${category}">
-    ${category}</label>`
-}
-
-
-
-
+// Para imprimir las cards
+printEvents(captureList);
 
 
 
--- assets/scripts/past.js
+++ assets/scripts/past.js
@@ -1,51 +1,49 @@
 // Para el Past Events
 
 // Para las categorías
-function printCategory() {
-    let allCateg = [];
-    let allCategPrint = [];
-    let container = document.getElementById("categoryId-past");
-
-    data.events.forEach(element => {
-        if (!allCateg.includes(element.category)) {
-            allCateg.push(element.category)
-        }
-    });
-
-    allCateg.forEach(element => {
-        allCategPrint.push(`<label for="${element}">
-        <input type="checkbox" name="choose" id="${element}" value="${element}">
-        ${element}</label>`)
-    })
-
-    container.innerHTML = allCategPrint.join("");
-}
-printCategory();
-
+printCategory("categoryId-past");
 
 // Para las cards
-function printPastEvents() {
-    let pastEvCards = [];
-    let containerPsEv = document.getElementById("card-PastEvents");
-
-    for (let event of data.events) {
+function createPastEventsArray(lista) {
+    let pastEvArray = [];
+    for (let event of lista) {
         if (event.date < data.currentDate) {
-            let card = `
-            <article class="card">
-                <figure>
-                    <img class="card-image" src="${event.image}" alt="${event.name}">
-                </figure>
-                <h2>${event.name}</h2>
-                <p>${event.description}</p>
-                <div class="base-card">
-                    <p>Price $${event.price}</p>
-                    <a class="but-view" href="./details.html?id=${event._id}">View more</a>
-                </div> 
-            </article>
-            `
-            pastEvCards.push(card);
+            pastEvArray.push(event);
         }
     }
-    containerPsEv.innerHTML = pastEvCards.join("");
+    printEvents(pastEvArray)
+    captureList = pastEvArray;
 }
-printPastEvents();
+
+// Llamo a la función para que me genere el array filtrado
+createPastEventsArray(data.events)
+
+
+
+
+
+// function printPastEvents() {
+//     let pastEvCards = [];
+//     let containerPsEv = document.getElementById("card-PastEvents");
+
+//     for (let event of data.events) {
+//         if (event.date < data.currentDate) {
+//             let card = `
+//             <article class="card">
+//                 <figure>
+//                     <img class="card-image" src="${event.image}" alt="${event.name}">
+//                 </figure>
+//                 <h2>${event.name}</h2>
+//                 <p>${event.description}</p>
+//                 <div class="base-card">
+//                     <p>Price $${event.price}</p>
+//                     <a class="but-view" href="./details.html?id=${event._id}">View more</a>
+//                 </div> 
+//             </article>
+//             `
+//             pastEvCards.push(card);
+//         }
+//     }
+//     containerPsEv.innerHTML = pastEvCards.join("");
+// }
+// printPastEvents();
